c20479c54f42c76fa2868cb8de3c95973e6bb8e6
A. Cody Schuffelen <schuffelen@google.com>
2020-03-06 20:48:48 -0800

    ANDROID: Add a boringssl variant of the openssl code
    Submitting this back upstream is TODO. Ideally it should be possible to
    change the openssl version upstream to be boringssl compatible.
    
    Implemented using the boringssl porting guide:
    https://boringssl.googlesource.com/boringssl/+show/471abb1/PORTING.md
    
    There are two main changes:
     - EVP_PKEY_HMAC is replaced with equivalent HMAC_* operations
     - TPm channel encryption using AES-CFB is removed.
    
    BoringSSL silently dropped AES-CFB in the first version and later added
    it back to its "decrepit" library, but I have not yet found an
    authoritative reason why it was deleted. As AES-CFB is baked into the
    TPM standard, this change in boringssl makes the tpm2-tss implementation
    strictly worse.
    
    It may be possible to make a case for putting tpm2-tss on an allow-list
    for boringssl decrepit, depending on the reason why it was originally
    removed.
    
    Bug: 148102516
    Test: New code is not built in this commit.
    Change-Id: I80c123e2f457dce3c83171f4861d3d0c04121d07

--- src/tss2-esys/esys_crypto_bssl.h
+++ src/tss2-esys/esys_crypto_bssl.h
@@ -3,8 +3,8 @@
  * Copyright 2017-2018, Fraunhofer SIT sponsored by Infineon Technologies AG
  * All rights reserved.
  ******************************************************************************/
-#ifndef ESYS_CRYPTO_OSSL_H
-#define ESYS_CRYPTO_OSSL_H
+#ifndef ESYS_CRYPTO_BSSL_H
+#define ESYS_CRYPTO_BSSL_H
 
 #include <stddef.h>
 #include "tss2_tpm2_types.h"
@@ -18,79 +18,79 @@ extern "C" {
 
 typedef struct _IESYS_CRYPTO_CONTEXT IESYS_CRYPTO_CONTEXT_BLOB;
 
-TSS2_RC iesys_cryptossl_hash_start(
+TSS2_RC iesys_cryptbssl_hash_start(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     TPM2_ALG_ID hashAlg);
 
-TSS2_RC iesys_cryptossl_hash_update(
+TSS2_RC iesys_cryptbssl_hash_update(
     IESYS_CRYPTO_CONTEXT_BLOB *context,
     const uint8_t *buffer, size_t size);
 
-TSS2_RC iesys_cryptossl_hash_update2b(
+TSS2_RC iesys_cryptbssl_hash_update2b(
     IESYS_CRYPTO_CONTEXT_BLOB *context,
     TPM2B *b);
 
-TSS2_RC iesys_cryptossl_hash_finish(
+TSS2_RC iesys_cryptbssl_hash_finish(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     uint8_t *buffer,
     size_t *size);
 
-TSS2_RC iesys_cryptossl_hash_finish2b(
+TSS2_RC iesys_cryptbssl_hash_finish2b(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     TPM2B *b);
 
-void iesys_cryptossl_hash_abort(IESYS_CRYPTO_CONTEXT_BLOB **context);
+void iesys_cryptbssl_hash_abort(IESYS_CRYPTO_CONTEXT_BLOB **context);
 
-#define iesys_crypto_pk_encrypt iesys_cryptossl_pk_encrypt
-#define iesys_crypto_hash_start iesys_cryptossl_hash_start
-#define iesys_crypto_hash_update iesys_cryptossl_hash_update
-#define iesys_crypto_hash_update2b iesys_cryptossl_hash_update2b
-#define iesys_crypto_hash_finish iesys_cryptossl_hash_finish
-#define iesys_crypto_hash_finish2b iesys_cryptossl_hash_finish2b
-#define iesys_crypto_hash_abort iesys_cryptossl_hash_abort
+#define iesys_crypto_pk_encrypt iesys_cryptbssl_pk_encrypt
+#define iesys_crypto_hash_start iesys_cryptbssl_hash_start
+#define iesys_crypto_hash_update iesys_cryptbssl_hash_update
+#define iesys_crypto_hash_update2b iesys_cryptbssl_hash_update2b
+#define iesys_crypto_hash_finish iesys_cryptbssl_hash_finish
+#define iesys_crypto_hash_finish2b iesys_cryptbssl_hash_finish2b
+#define iesys_crypto_hash_abort iesys_cryptbssl_hash_abort
 
-TSS2_RC iesys_cryptossl_hmac_start(
+TSS2_RC iesys_cryptbssl_hmac_start(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     TPM2_ALG_ID hmacAlg,
     const uint8_t *key,
     size_t size);
 
-TSS2_RC iesys_cryptossl_hmac_start2b(
+TSS2_RC iesys_cryptbssl_hmac_start2b(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     TPM2_ALG_ID hmacAlg,
     TPM2B *b);
 
-TSS2_RC iesys_cryptossl_hmac_update(
+TSS2_RC iesys_cryptbssl_hmac_update(
     IESYS_CRYPTO_CONTEXT_BLOB *context,
     const uint8_t *buffer,
     size_t size);
 
-TSS2_RC iesys_cryptossl_hmac_update2b(
+TSS2_RC iesys_cryptbssl_hmac_update2b(
     IESYS_CRYPTO_CONTEXT_BLOB *context,
     TPM2B *b);
 
-TSS2_RC iesys_cryptossl_hmac_finish(
+TSS2_RC iesys_cryptbssl_hmac_finish(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     uint8_t *buffer,
     size_t *size);
 
-TSS2_RC iesys_cryptossl_hmac_finish2b(
+TSS2_RC iesys_cryptbssl_hmac_finish2b(
     IESYS_CRYPTO_CONTEXT_BLOB **context,
     TPM2B *b);
 
-void iesys_cryptossl_hmac_abort(IESYS_CRYPTO_CONTEXT_BLOB **context);
+void iesys_cryptbssl_hmac_abort(IESYS_CRYPTO_CONTEXT_BLOB **context);
 
-#define iesys_crypto_hmac_start iesys_cryptossl_hmac_start
-#define iesys_crypto_hmac_start2b iesys_cryptossl_hmac_start2b
-#define iesys_crypto_hmac_update iesys_cryptossl_hmac_update
-#define iesys_crypto_hmac_update2b iesys_cryptossl_hmac_update2b
-#define iesys_crypto_hmac_finish iesys_cryptossl_hmac_finish
-#define iesys_crypto_hmac_finish2b iesys_cryptossl_hmac_finish2b
-#define iesys_crypto_hmac_abort iesys_cryptossl_hmac_abort
+#define iesys_crypto_hmac_start iesys_cryptbssl_hmac_start
+#define iesys_crypto_hmac_start2b iesys_cryptbssl_hmac_start2b
+#define iesys_crypto_hmac_update iesys_cryptbssl_hmac_update
+#define iesys_crypto_hmac_update2b iesys_cryptbssl_hmac_update2b
+#define iesys_crypto_hmac_finish iesys_cryptbssl_hmac_finish
+#define iesys_crypto_hmac_finish2b iesys_cryptbssl_hmac_finish2b
+#define iesys_crypto_hmac_abort iesys_cryptbssl_hmac_abort
 
-TSS2_RC iesys_cryptossl_random2b(TPM2B_NONCE *nonce, size_t num_bytes);
+TSS2_RC iesys_cryptbssl_random2b(TPM2B_NONCE *nonce, size_t num_bytes);
 
-TSS2_RC iesys_cryptossl_pk_encrypt(
+TSS2_RC iesys_cryptbssl_pk_encrypt(
     TPM2B_PUBLIC *key,
     size_t in_size,
     BYTE *in_buffer,
@@ -100,7 +100,7 @@ TSS2_RC iesys_cryptossl_pk_encrypt(
     const char *label);
 
 
-TSS2_RC iesys_cryptossl_sym_aes_encrypt(
+TSS2_RC iesys_cryptbssl_sym_aes_encrypt(
     uint8_t *key,
     TPM2_ALG_ID tpm_sym_alg,
     TPMI_AES_KEY_BITS key_bits,
@@ -110,7 +110,7 @@ TSS2_RC iesys_cryptossl_sym_aes_encrypt(
     size_t dst_size,
     uint8_t *iv);
 
-TSS2_RC iesys_cryptossl_sym_aes_decrypt(
+TSS2_RC iesys_cryptbssl_sym_aes_decrypt(
     uint8_t *key,
     TPM2_ALG_ID tpm_sym_alg,
     TPMI_AES_KEY_BITS key_bits,
@@ -120,7 +120,7 @@ TSS2_RC iesys_cryptossl_sym_aes_decrypt(
     size_t dst_size,
     uint8_t *iv);
 
-TSS2_RC iesys_cryptossl_get_ecdh_point(
+TSS2_RC iesys_cryptbssl_get_ecdh_point(
     TPM2B_PUBLIC *key,
     size_t max_out_size,
     TPM2B_ECC_PARAMETER *Z,
@@ -128,14 +128,14 @@ TSS2_RC iesys_cryptossl_get_ecdh_point(
     BYTE * out_buffer,
     size_t * out_size);
 
-#define iesys_crypto_random2b iesys_cryptossl_random2b
-#define iesys_crypto_get_ecdh_point iesys_cryptossl_get_ecdh_point
-#define iesys_crypto_sym_aes_encrypt iesys_cryptossl_sym_aes_encrypt
-#define iesys_crypto_sym_aes_decrypt iesys_cryptossl_sym_aes_decrypt
+#define iesys_crypto_random2b iesys_cryptbssl_random2b
+#define iesys_crypto_get_ecdh_point iesys_cryptbssl_get_ecdh_point
+#define iesys_crypto_sym_aes_encrypt iesys_cryptbssl_sym_aes_encrypt
+#define iesys_crypto_sym_aes_decrypt iesys_cryptbssl_sym_aes_decrypt
 
-TSS2_RC iesys_cryptossl_init();
+TSS2_RC iesys_cryptbssl_init();
 
-#define iesys_crypto_init iesys_cryptossl_init
+#define iesys_crypto_init iesys_cryptbssl_init
 
 #ifdef __cplusplus
 } /* extern "C" */
